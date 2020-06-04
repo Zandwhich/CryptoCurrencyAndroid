c6e1f1643ce2aa76ae9a694d2501e937ee4abd97
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2020-06-04 01:06:00 -0700

    Added some strings and started linking the activity with the layout

--- app/src/main/java/com/example/phone/activities/AboutActivity.java
+++ app/src/main/java/com/example/phone/activities/AboutActivity.java
@@ -1,4 +1,36 @@
 package com.example.phone.activities;
 
-public class AboutActivity {
-}
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.phone.R;
+import com.example.phone.utility.network.AbstractAPICall;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * The class that controls the about page
+ */
+final public class AboutActivity extends AppCompatActivity {
+
+    private TextView mTVName;
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        this.mTVName = findViewById(R.id.tv_about_name);
+    }//end onCreate()
+
+    public AboutActivity(AbstractAPICall call) {
+        super();
+
+
+    }//end AboutActivity()
+}//end AboutActivity
