1d8a84cea64b0bb7e0ae55754236a880b3088ce5
Alex Zdanowicz <alex.zdanowiz@gmail.com>
2020-06-11 16:15:09 -0700

    Added some comments to the view holder

--- app/src/main/java/com/example/phone/activities/recyclerview/PriceAdapter.java
+++ app/src/main/java/com/example/phone/activities/recyclerview/PriceAdapter.java
@@ -16,32 +16,54 @@ import com.example.phone.R;
  */
 public final class PriceAdapter extends RecyclerView.Adapter<PriceAdapter.PriceViewHolder> {
 
+    // TODO: Learn about RecyclerViews more and better understand how all of this works
+
     /**
      * The simple name of the class used for displaying logs
      */
     private static final String TAG = PriceAdapter.class.getSimpleName();
 
+    /**
+     * The total number of items that this recyclerview has
+     */
     private int mNumberItems;
 
     public PriceAdapter(int numberOfItems) {
         this.mNumberItems = numberOfItems;
     }//end PriceAdapter()
 
+    /**
+     * Runs when the view holder is created? I think
+     * TODO: Figure out what this does, lol
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull
     @Override
     public PriceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        // TODO: Get rid of this constant here (the boolean)
         View view = inflater.inflate(R.layout.price_list_item, parent, false);
 
         return new PriceViewHolder(view);
     }//end onCrateViewHolder()
 
+    /**
+     * TODO: Figure out how to pass in actual price details
+     * Will bind the {@link PriceViewHolder} with the given position
+     * @param holder The {@link PriceViewHolder} that will display all of the data
+     * @param position The given position of that view holder
+     */
     @Override
     public void onBindViewHolder(@NonNull PriceViewHolder holder, int position) {
         Log.d(PriceAdapter.TAG, "#" + position);
         holder.bind(position);
     }//end onBindViewHolder()
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getItemCount() {
         return this.mNumberItems;
@@ -70,6 +92,7 @@ public final class PriceAdapter extends RecyclerView.Adapter<PriceAdapter.PriceV
         }//end PriceViewHolder()
 
         /**
+         * TODO: Figure out how to properly display the price information
          * The method that is called to bind this specific view holder to its parent
          * @param listIndex The given position of the view holder
          */
